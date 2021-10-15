d3c016cb365fdf787565db8cb6f2c95283970aec
martonesantana <martonesantos@hotmail.com>
2021-10-15 09:59:15 -0300

    Ajuste do json one to many

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,26 +1,32 @@
 from rest_framework import fields, serializers
 from core import models
 
+
 class SituacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Situacao
         fields = '__all__'
 
+
 class CausaSerializer(serializers.ModelSerializer):
+    situacoes = SituacaoSerializer(many=True)
+
     class Meta:
         model = models.Causa
         fields = '__all__'
 
 
-class DetalheAcaoSerializer(serializers.ModelSerializer):
-    acoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True) 
-    
-    class Meta:
-        model = models.DetalheAcao
-        fields = '__all__'
-
 class AcaoSerializer(serializers.ModelSerializer):
-    feeds = DetalheAcaoSerializer(many=True, read_only=True)
+    causas = CausaSerializer(many=True)
+
     class Meta:
         model = models.Acao
+        fields = '__all__'
+
+
+class DetalheAcaoSerializer(serializers.ModelSerializer):
+    acoes = AcaoSerializer(many=True)
+
+    class Meta:
+        model = models.DetalheAcao
         fields = '__all__'
